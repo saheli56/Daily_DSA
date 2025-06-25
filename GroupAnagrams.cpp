58ca09960c93e5a907d2116ed5c73a837825eca2
Saheli Das <sahelidas0506@gmail.com>
2025-06-25 14:07:45 +0530

    Added Group Anagrams.cpp

---
+++ GroupAnagrams.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+       unordered_map<string,vector<string>> anagramMap;
+       for (string str : strs){
+         string sortedStr = str;
+            sort(sortedStr.begin(), sortedStr.end());  
+            anagramMap[sortedStr].push_back(str);
+       }
+        vector<vector<string>> result;
+        for (auto& pair : anagramMap) {
+            result.push_back(pair.second);
+        }
+
+        return result;
+    }
+};
