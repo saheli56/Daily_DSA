dae8773fa8dd8d219b58695644f423240a331d3d
Saheli Das <sahelidas0506@gmail.com>
2025-06-30 22:19:13 +0530

    Added Majority Element.cpp

---
+++ MajorityElement.cpp
@@ -0,0 +1,11 @@
+ int count = 0;
+        int candidate;
+        
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+        
+        return candidate;
